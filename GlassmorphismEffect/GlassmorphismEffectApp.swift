3678d8db079c33c8631a32f14c597a739fdbb0c8
Amish <amish.tufail2002@gmail.com>
2024-02-03 13:01:23 +0500

    Initial Commit

---
+++ GlassmorphismEffect/GlassmorphismEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismEffectApp.swift
+//  GlassmorphismEffect
+//
+//  Created by Amish Tufail on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
